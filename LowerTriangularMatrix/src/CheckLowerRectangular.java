d2eb153fccfffa9249cc060d197933bf7cd88227
Itisha <71888338+itisha2808@users.noreply.github.com>
2021-05-29 23:05:03 +0530

    Add files via upload

---
+++ LowerTriangularMatrix/src/CheckLowerRectangular.java
@@ -0,0 +1,8 @@
+public class CheckLowerRectangular {
+
+    public static void main(String[] args){
+
+        LowerTriangularMatrix matrix1=new LowerTriangularMatrix();
+        matrix1.isUpperRect();
+    }
+}
